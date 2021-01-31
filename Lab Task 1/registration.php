c1f7300679b4895eadc1ddb5b59fa038d81ba015
Moontasirul Islam <78253802+MoontasirulIslam@users.noreply.github.com>
2021-01-31 17:11:38 +0600

    Lab Task 1

---
+++ Lab Task 1/registration.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Registration Page</title>
+</head>
+<body>
+<h1>REGISTRATION</h1>
+
+<form>
+<table>
+<tr> <td>Name</td>
+<td>:<input type="text" name="name"></td>
+</tr>
+
+<tr> <td>Email</td>
+<td>:<input type="text" name="email"></td>
+</tr>
+
+<tr> <td>User Name</td>
+<td>:<input type="text" name="username"></td>
+</tr>
+
+<tr> <td>Password</td>
+<td>:<input type="password" name="password"></td>
+</tr>
+
+<tr> <td>Confirm Password</td>
+<td>:<input type="password" name="confirmpassword"></td>
+</tr>
+
+<tr><td>Gender</td>
+</tr>
+
+<tr><td><input type="radio" name="gender">Male <input type="radio" name="gender">Female <input type="radio" name="gender">Other</td>
+</tr>
+
+<tr><td>Date of Birth</td>
+</tr>
+
+<tr><td><input type="date" name="dd" size=1>(dd/mm/yyyy)</td>
+</tr>
+<tr></tr>
+<tr>
+<td><input type="button" value="Submit"> <input type="button" value="Reset"></td>
+</tr>
+</table>
+</form>
+</body>
+</html>
